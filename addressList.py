74b235ab8559e9f75ad142131fd47796ecc18fe4
Sheng Yuan <peter_office@163.com>
2018-04-14 08:49:27 +0800

    update

--- addressList.py
+++ addressList.py
@@ -20,32 +20,37 @@ class person:
 
         直接使用self
         '''
-        f = open(person.addressfile, "wbr")
+
         diction = {}
-        # if os.path.exists(person.addressfile):
-        try:
-            diction = p.load(f)
-            print(diction)
-        except:
-            pass
+        if os.path.exists(person.addressfile):
+            with open(person.addressfile, "rb+") as f:
+                try:
+                    diction = p.load(f)
+                    print(diction)
+                except Exception as e:
+                    print(Exception, e)
         diction[self.name] = self
         print(diction)
-        p.dump(diction, f)
-        f.close()
+        with open(person.addressfile, "rb+") as f:
+            p.dump(diction, f)
 
 
 def delete(name):
 
-    with open(person.addressfile, "rwb") as f:
+    with open(person.addressfile, "rb+") as f:
         dict = p.load(f)
         print(p)
         dict.pop(name)
         p.dump(dict, f)
 
 def show():
-    with open(person.addressfile, "rb") as f:
-        data = p.load(f)
-        print(data)
+    with open(person.addressfile, "rb+") as f:
+        try:
+            data = p.load(f)
+            print(data)
+        except Exception as e:
+            print(Exception, e)
+
 
 
 def main():
@@ -64,14 +69,14 @@ def cmdhandle(cmd):
         print("=== Bye ===")
         sys.exit()
     elif cmd == "add":
-        name = raw_input("Please input name:")
-        address = raw_input("Please input address:")
+        name = input("Please input name:")
+        address = input("Please input address:")
         phone = input("Please input phone:")
-        email = raw_input("Please input email:")
+        email = input("Please input email:")
         p = person(name, address, phone, email)
         p.dump()
     elif cmd == "delete":
-        name = raw_input("Please input name:")
+        name = input("Please input name:")
         delete(name)
     elif cmd == "show":
         show()
@@ -81,5 +86,5 @@ def cmdhandle(cmd):
 if __name__ == "__main__":
     print("=== welcome to library ===")
     while(True):
-        cmd = raw_input(">> ")
+        cmd = input(">> ")
         cmdhandle(cmd)
